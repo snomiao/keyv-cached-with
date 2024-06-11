ece805c15df6dc4616cf1c430eb543aa97d914b9
snomiao <snomiao@gmail.com>
2024-06-11 07:00:30 +0000

    feat(main): curry
    curry

--- index.spec.ts
+++ index.spec.ts
@@ -1,44 +1,91 @@
 import Keyv from "keyv";
-import { KeyvCachedWith } from "./index";
+import { KeyvCachedWith as fn } from "./index";
+import { expectTypeOf } from "expect-type";
+it("works in slot3", async () => {
+  const r1 = await fn(new Keyv(), async (a: string) => 123, "asdf");
+  expectTypeOf(r1).toEqualTypeOf<any>(); // because of Keyv<Value = any>
+  const r2 = await fn(new Keyv(), async (a: number) => 123, 1234);
+  expectTypeOf(r2).toEqualTypeOf<any>(); // because of Keyv<Value = any>
+  const r3 = await fn(new Keyv<string>(), async (a: number) => "123", 1234);
+  expectTypeOf(r3).toEqualTypeOf<string>();
+  const r4 = await fn(new Keyv<number>(), async (a: number) => 123, 1234);
+  expectTypeOf(r4).toEqualTypeOf<number>();
+  const r5 = await fn(new Keyv<unknown>(), async (a: number) => "123", 1234);
+  expectTypeOf(r5).toEqualTypeOf<string>();
+  const r6 = await fn(new Keyv<number>(), async (a: number) => 123, 1234);
+  expectTypeOf(r6).toEqualTypeOf<number>();
+  const r9 = await fn(new Keyv<unknown>(), async (a) => 1 as unknown, 1234);
+  expectTypeOf(r9).toEqualTypeOf<unknown>();
 
-it("works in full-args call", async () => {
+  // num
+  const r7: number = await fn(new Keyv<unknown>(), async () => 123);
+  expectTypeOf(r7).toEqualTypeOf<number>();
+  // obj
+  const r8: { a: number } = await fn(new Keyv<unknown>(), () => ({ a: 123 }));
+  expectTypeOf(r8).toEqualTypeOf<{ a: number }>();
+});
+it("works in slot2", async () => {
   let i = 0;
-  const kv = new Keyv();
-  const fn1 = KeyvCachedWith(kv, async (a: number) => `${a}/${++i}`) satisfies (
-    a: number
-  ) => Promise<string>;
-  expect(await fn1(1)).toBe("1/1");
-  expect(await fn1(5)).toBe("5/2");
-  expect(await fn1(1)).toBe("1/1");
+  const kv = new Keyv<unknown>();
+  const f1 = fn(kv, async (a: number) => `${a}/${++i}`);
+  expectTypeOf(f1).not.toEqualTypeOf<any>();
+  expectTypeOf(f1).not.toEqualTypeOf<unknown>();
+  expect(await f1(1)).toBe("1/1");
+  expect(await f1(5)).toBe("5/2");
+  expect(await f1(1)).toBe("1/1");
+  expectTypeOf(await f1(1)).toEqualTypeOf<string>();
 
-  const fn2 = KeyvCachedWith(kv, async (a: number) => `${a}/${++i}`);
-  expect(await fn2(1)).toBe("1/1");
-  expect(await fn2(5)).toBe("5/2");
-  expect(await fn2(1)).toBe("1/1");
+  const f2 = fn(kv, async (a: number) => `${a}/${++i}`);
+  expectTypeOf(f2).not.toEqualTypeOf<(...args: any) => Promise<any>>();
+  expectTypeOf(f2).not.toEqualTypeOf<any>();
+  expectTypeOf(f2).not.toEqualTypeOf<unknown>();
+  expect(await f2(1)).toBe("1/1");
+  expect(await f2(5)).toBe("5/2");
+  expectTypeOf(await f2(1)).toEqualTypeOf<string>();
+  expect(await f2(1)).toBe("1/1");
+
+  const f3 = fn(new Keyv(), async (a: number) => 123);
+  await f3(1234);
+  const f4 = fn(new Keyv(), async (a) => 123);
+  await f4("zxcv");
+
+  const fStr1 = fn(new Keyv<string>(), <T>(a: T) => a);
+  expectTypeOf(await fStr1("abc")).toEqualTypeOf<string>();
+  const fStr2 = fn(new Keyv<unknown>(), (a: string) => a);
+  expectTypeOf(await fStr2("abc")).toEqualTypeOf<string>();
+  const fStr3 = fn(new Keyv<unknown>(), (a) => String(a));
+  expectTypeOf(await fStr3("abc")).toEqualTypeOf<string>();
+  expectTypeOf(await fStr3(123)).toEqualTypeOf<string>();
+
+  const fUnknown = fn(new Keyv<unknown>(), <T>(a: T) => a);
+  const abc = await fUnknown("abc");
+  expectTypeOf(abc).toEqualTypeOf<unknown>();
+
+  const fAny = fn(new Keyv(), <T>(a: T) => a);
+  expectTypeOf(await fAny("abc")).toEqualTypeOf<any>();
+  const fAny2 = fn(new Keyv<unknown>(), <T>(a: T) => a as any);
+  expectTypeOf(await fAny2("abc")).toEqualTypeOf<any>();
+  const fAny3 = fn(new Keyv<unknown>(), <T>(a: T) => a);
+  expectTypeOf(await fAny3("abc" as any)).toEqualTypeOf<unknown>();
 });
 
-it("works with curry", async () => {
-  const CachedWith = KeyvCachedWith(new Keyv());
+it("works in slot1", async () => {
   let i = 0;
+  const CachedWith = fn(new Keyv<string>());
+  const f2 = CachedWith((a: number) => `${a}/${++i}`);
+  expectTypeOf(f2).not.toEqualTypeOf<any>();
+  expectTypeOf(f2).not.toEqualTypeOf<unknown>();
+  expectTypeOf(await f2(123)).toEqualTypeOf<string>();
+  expect(await f2(2)).toBe("2/1");
+  expect(await f2(5)).toBe("5/2");
+  expect(await f2(2)).toBe("2/1"); // use cache
 
-  const fn2 = CachedWith((a: number) => `${a}/${++i}`) satisfies (
-    a: number
-  ) => Promise<string>;
-  expect(await fn2(2)).toBe("2/1");
-  expect(await fn2(5)).toBe("5/2");
-  expect(await fn2(2)).toBe("2/1"); // use cache
-
-  // fn3's content is same with fn2
-  const fn3 = CachedWith((a: number) => `${a}/${++i}`) satisfies (
-    a: number
-  ) => Promise<string>;
-  expect(await fn3(2)).toBe("2/1"); // use cache
-  expect(await fn3(5)).toBe("5/2"); // use cache
-  expect(await fn3(6)).toBe("6/3"); // eval
-
-  // fn4's content is different
-  const fn4 = CachedWith((a: number) => `${a}/${++i}` + "".trim());
-  expect(await fn4(2)).toBe("2/4"); // use cache
-  expect(await fn4(5)).toBe("5/5");
-  expect(await fn4(2)).toBe("2/4"); // use cache
+  const CachedWith1 = fn(new Keyv<string>());
+  const v1 = await CachedWith1((a: number) => `${a}/${++i}`, 123);
+  expectTypeOf(v1).toBeString();
+  const CachedWith2 = fn(new Keyv<unknown>());
+  const v2 = await CachedWith2((a: number) => `${a}/${++i}`, 123);
+  expectTypeOf(v2).toBeString();
+  const v3 = await CachedWith2((a: string) => `${a}/${++i}`, "123");
+  expectTypeOf(v3).toBeString();
 });
--- index.ts
+++ index.ts
@@ -1,29 +1,50 @@
 import Keyv from "keyv";
 import md5 from "md5";
 import { curry } from "rambda";
-
-export function KeyvCachedWith<V, Fn extends (...args: any) => V>(
-  keyv: Keyv<V>,
-  fn: Fn
-): (...args: Parameters<Fn>) => Promise<Awaited<ReturnType<Fn>>>;
-export function KeyvCachedWith<V>(
-  keyv: Keyv<V>
-): <Fn extends (...args: any) => V>(
-  fn: Fn
-) => (...args: Parameters<Fn>) => Promise<Awaited<ReturnType<Fn>>>;
-export function KeyvCachedWith<
-  Fn extends (...args: any) => any,
-  V extends ReturnType<Fn> = ReturnType<Fn>
->(keyv: Keyv<V>, fn?: Fn) {
-  if (!fn) return curry(KeyvCachedWith)(keyv);
-  return async (...args: Parameters<Fn>) => {
-    const key =
-      md5(String(fn)).slice(0, 8) +
-      md5(JSON.stringify(args.slice(0, fn.length))).slice(0, 8);
-    const cache = (await keyv.get(key)) as V;
-    if (cache) return cache;
-    const result = (await fn(...args)) as V;
-    await keyv.set(key, result);
-    return result;
-  };
+import type { Function } from "ts-toolbelt/out/Function/Function";
+// util util util util util util util util util util util util util util util
+type Awaitable<R> = Promise<R> | R;
+type Repromise<T> = Promise<Awaited<T>>;
+type KeyV<KV extends Keyv> = KV extends Keyv<infer R> ? Awaitable<R> : never;
+// type type type type type type type type type type type type type type type
+type $<
+  KV extends Keyv = Keyv,
+  FN extends Function<any[], KeyV<KV>> = Function<any, KeyV<KV>>,
+  AR extends Parameters<FN> = Parameters<FN>,
+  RE extends Repromise<ReturnType<FN> & KeyV<KV>> = Repromise<
+    ReturnType<FN> & KeyV<KV>
+  >
+> = [RE, KV, FN, AR];
+// impl impl impl impl impl impl impl impl impl impl impl impl impl impl impl
+async function _<
+  A extends $[1],
+  B extends $<A>[2],
+  C extends $<A, B>[3],
+  Z extends $<A, B, C>[0]
+>(keyv: A, fn: B, ...args: C): Promise<Z> {
+  const key =
+    md5(String(fn)).slice(0, 8) +
+    md5(JSON.stringify(args.slice(0, fn.length))).slice(0, 8);
+  const cache = await keyv.get(key);
+  if (cache) return cache;
+  const result = await fn(...args);
+  await keyv.set(key, result);
+  return result;
 }
+// export export export export export export export export export export expo
+// prettier-ignore
+export const KeyvCachedWith = curry(_) as
+  (<A extends $[1], B extends $<A>[2], C extends $<A, B>[3]>
+    (keyv: A, fn: B, ...args: C) =>                           $<A, B, C>[0]) &
+  (<A extends $[1], B extends $<A>[2]>
+    (keyv: A, fn: B) =>
+      <C extends $<A, B>[3]>
+        (...args: C) =>                                       $<A, B, C>[0]) &
+  (<A extends $[1]>
+    (keyv: A) => (
+      (<B extends $<A>[2], C extends $<A, B>[3]>
+        (fn: B, ...args: C) =>                                $<A, B, C>[0]) &
+      (<B extends $<A>[2]>
+        (fn: B) =>
+          <C extends $<A, B>[3]>
+            (...args: C) =>                                   $<A, B, C>[0])))
